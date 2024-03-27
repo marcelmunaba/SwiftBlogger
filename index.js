661754aeb8929c73f3990057cacf841d76d56483
--global <marcel.munaba@tum.de>
2024-03-27 23:32:00 +0100

    added partials templating

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
   res.render("index.ejs");
 });
+
+//TODO : routing for post requests(create a post)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
