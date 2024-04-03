914df15a42b4dd62cc2faa1690314c5544ab9d2c
--global <marcel.munaba@tum.de>
2024-04-03 14:12:49 +0700

    Added weather/city + geolocation APIs

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+//TODO: geolocation API first 
+const GEOLOCATION_API_URL = "http://ip-api.com/json";
+//Weather data fetch
+var API_URL = "https://api.openweathermap.org/data/2.5/weather?";
 //Post Object
-function BlogPost (title, content, date) {
+function BlogPost (title, content, date, city) {
   this.title = title;
   this.content = content;
   this.date = date;
+  this.city = city;
 }
 
 //List containing all of the posts created.
@@ -18,31 +24,54 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+// Fetch geolocation data
+const location = await axios.get(GEOLOCATION_API_URL);
+
+// Fetch weather / Temperature data
+const result = await axios.get(API_URL + "q="+location.data.city+"&units=metric&appid=b0e987f4221739548c7feb541c0ce18a");
+
+app.get("/", async (req, res) => {
   res.render("index.ejs", {
-    list:postList
+    list:postList,
+    weather: result.data.weather[0].main,
+    city: result.data.name,
+    temp: result.data.main.temp,
+    icon: result.data.weather[0].icon
   });
 });
 
 //Features : post creation, post viewing, post update/delete, styling
 //POST CREATION
 app.get("/create", (req, res) => {
-  res.render("create.ejs");
+  res.render("create.ejs", {
+    weather: result.data.weather[0].main,
+    city: result.data.name,
+    temp: result.data.main.temp,
+    icon: result.data.weather[0].icon
+  });
 });
 
 app.get("/posts", (req, res) => {
   res.render("posts.ejs", {
-    list:postList
+    list:postList,
+    weather: result.data.weather[0].main,
+    city: result.data.name,
+    temp: result.data.main.temp,
+    icon: result.data.weather[0].icon
   });
 });
 
 app.post("/submit", (req, res) => {
-  var blogPost = new BlogPost(req.body["title"], req.body["content"], new Date());
+  var blogPost = new BlogPost(req.body["title"], req.body["content"], new Date(), result.data.name);
   postList.push(blogPost);
   console.log(blogPost);
   console.log(postList);
   res.render("posts.ejs", {
-    list:postList
+    list:postList,
+    weather: result.data.weather[0].main,
+    city: result.data.name,
+    temp: result.data.main.temp,
+    icon: result.data.weather[0].icon
   });
 })
 
@@ -51,13 +80,17 @@ app.get("/edit/:index", (req,res) => {
   const index = req.params.index;
   res.render("edit.ejs", {
     post: postList[index],
-    index: index
+    index: index,
+    weather: result.data.weather[0].main,
+    city: result.data.name,
+    temp: result.data.main.temp,
+    icon: result.data.weather[0].icon
   });
 })
 
 app.post("/edit_post", (req,res) => {
   const index = req.body.index;
-  const updatedPost = new BlogPost(req.body["title"], req.body["content"], new Date());
+  const updatedPost = new BlogPost(req.body["title"], req.body["content"], new Date(), result.data.name);
   postList[index] = updatedPost;
   res.redirect("/posts");
 })
